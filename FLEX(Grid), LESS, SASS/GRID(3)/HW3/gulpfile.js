23683e35f6cd6d9691da4b708aafa3bbd16c5b1d
Your Name <you@example.com>
2018-10-29 14:21:46 +0300

    add grid3

---
+++ FLEX(Grid), LESS, SASS/GRID(3)/HW3/gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const gcmq = require('gulp-group-css-media-queries');
+
+const config = {
+    src: './src',
+    css: {
+        src: '/precss/**/*.css',
+        dest: '/css'
+    },
+    html: {
+        src: '/index.html'
+    }
+};
+
+gulp.task('build', function(){
+    gulp.src(config.src + config.css.src)
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['> 0.1%'],
+            cascade: false
+        }))
+        .pipe(gcmq())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.src + config.css.dest))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+    
+    gulp.src(config.src + config.html.src)
+        .pipe(gulp.dest(config.src))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', ['browserSync'], function(){
+    gulp.watch(config.src + config.css.src, ['build']);
+    gulp.watch(config.src + config.html.src, ['build']);
+});
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        server: {
+            baseDir: config.src
+        }
+    });
+});
