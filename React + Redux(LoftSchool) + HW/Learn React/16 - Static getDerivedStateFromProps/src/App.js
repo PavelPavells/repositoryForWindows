a3fbbc2244fd18734bd490fa4da4dd5dbd65d721
Your Name <you@example.com>
2018-11-11 17:43:48 +0300

    add lesson16 React

---
+++ React + Redux(LoftSchool) + HW/Learn React/16 - Static getDerivedStateFromProps/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Add } from './components/Add'
+import { News } from './components/News'
+import './App.css'
+
+class App extends React.Component {
+  state = {
+    news: null,
+    isLoading: false,
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/newsData.json')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setTimeout(() => {
+          this.setState({ isLoading: false, news: data })
+        }, 1000)
+      })
+  }
+  handleAddNews = data => {
+    const nextNews = [data, ...this.state.news]
+    this.setState({ news: nextNews })
+  }
+  render() {
+    const { news, isLoading } = this.state
+
+    return (
+      <React.Fragment>
+        <Add onAddNews={this.handleAddNews} />
+        <h3>Новости</h3>
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
