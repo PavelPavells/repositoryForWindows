b30f17710d79f89b2f18f381793a96fb1245dd13
Your Name <you@example.com>
2018-11-12 14:49:53 +0300

    add lesson2 Redux

---
+++ React + Redux(LoftSchool) + HW/Learn Redux/02 - Point of Entry/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Мой топ фото</h1>
+        </header>
+        <p className="App-intro">Здесь будут мои самые залайканые фото</p>
+      </div>
+    )
+  }
+}
+
+export default App
