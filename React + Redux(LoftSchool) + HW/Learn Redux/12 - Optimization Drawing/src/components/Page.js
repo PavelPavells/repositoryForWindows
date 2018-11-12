0766a5bf0a54b36bc427e67f03ccd74706caefb8
Your Name <you@example.com>
2018-11-12 17:02:03 +0300

    add lesson12 Redux

---
+++ React + Redux(LoftSchool) + HW/Learn Redux/12 - Optimization Drawing/src/components/Page.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class Page extends React.Component {
+  onBtnClick = e => {
+    const year = +e.currentTarget.innerText
+    this.props.getPhotos(year) // setYear -> getPhotos
+  }
+  renderButtons = () => {
+    const years = [2018, 2017, 2016, 2015, 2014]
+
+    return years.map((item, index) => {
+      return (
+        <button key={index} className="btn" onClick={this.onBtnClick}>
+          {item}
+        </button>
+      )
+    })
+  }
+  renderTemplate = () => {
+    const { photos, isFetching, error } = this.props
+
+    if (error) {
+      return <p className="error">Во время загрузки фото произошла ошибка</p>
+    }
+
+    if (isFetching) {
+      return <p>Загрузка...</p>
+    } else {
+      return photos.map(entry => (
+        <div key={entry.id} className="photo">
+          <p>
+            <img src={entry.sizes[0].url} alt="" />
+          </p>
+          <p>{entry.likes.count} ❤</p>
+        </div>
+      ))
+    }
+  }
+
+  render() {
+    const { year, photos } = this.props
+    return (
+      <div className="ib page">
+        <p>{this.renderButtons()}</p>
+        <h3>
+          {year} год [{photos.length}]
+        </h3>
+        {this.renderTemplate()}
+      </div>
+    )
+  }
+}
+
+Page.propTypes = {
+  year: PropTypes.number.isRequired,
+  photos: PropTypes.array.isRequired,
+  getPhotos: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+}
