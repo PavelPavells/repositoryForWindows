4688b31691937a4bf02517e8003caa3dbb921e65
Your Name <you@example.com>
2018-11-12 16:45:08 +0300

    add lesson11 Redux

---
+++ React + Redux(LoftSchool) + HW/Learn Redux/11 - VK Api/src/components/Page.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export class Page extends React.Component {
+  onBtnClick = e => {
+    const year = +e.currentTarget.innerText
+    this.props.getPhotos(year) // setYear -> getPhotos
+  }
+  renderTemplate = () => {
+    const { photos, isFetching, error } = this.props
+
+    if (error) {
+      return <p className="error">Во время загрузки фото произошла ошибка</p>
+    }
+
+    if (isFetching) {
+      return <p>Загрузка...</p>
+    } else {
+      return photos.map(entry => (
+        <div key={entry.id} className="photo">
+          <p>
+            <img src={entry.sizes[0].url} alt="" />
+          </p>
+          <p>{entry.likes.count} ❤</p>
+        </div>
+      ))
+    }
+  }
+
+  render() {
+    const { year, photos } = this.props
+    return (
+      <div className="ib page">
+        <p>
+          <button className="btn" onClick={this.onBtnClick}>
+            2018
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2017
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2016
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2015
+          </button>{' '}
+          <button className="btn" onClick={this.onBtnClick}>
+            2014
+          </button>
+        </p>
+        <h3>
+          {year} год [{photos.length}]
+        </h3>
+        {this.renderTemplate()}
+      </div>
+    )
+  }
+}
+
+Page.propTypes = {
+  year: PropTypes.number.isRequired,
+  photos: PropTypes.array.isRequired,
+  getPhotos: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+}
